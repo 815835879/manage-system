8dfa83be878317f45c9df01ee45cb6f82a0bec98
liubo <815835879@qq.com>
2019-12-04 17:33:44 +0800

    utils to do

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,37 @@
 import React from 'react'
+import { Row, Col } from 'antd'
+import './index.less'
 
 export default class Header extends React.Component {
+  state = {
+    userName: 'eric',
+    sysTime: '2019-12-4 16:05',
+    dayPictureUrl: '',
+    weather: '晴转多云'
+  }
   render() {
     return(
-      <div>
-        header
+      <div className="header">
+        <Row className="header-top">
+          <Col span="24">
+            <span>欢迎，{this.state.userName}</span>
+            <a href="#">退出</a>
+          </Col>
+        </Row>
+        <Row className="breadcrumb">
+          <Col span="4" className="breadcrumb-title">
+            首页
+          </Col>
+          <Col span="20" className="weather">
+            <span className="date">{this.state.sysTime}</span>
+            <span className="weather-img">
+              <img src={this.state.dayPictureUrl} alt="" />
+            </span>
+            <span className="weather-detail">
+              {this.state.weather}
+            </span>
+          </Col>
+        </Row>
       </div>
     )
   }
