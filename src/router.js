ad10f919d97208fbfcb28d4180701d6021b4713a
liubo <815835879@qq.com>
2019-12-12 18:59:29 +0800

    login to do

--- src/router.js
+++ src/router.js
@@ -10,6 +10,8 @@ import Loadings from './pages/ui/loadings'
 import Notice from './pages/ui/notice'
 import Messages from './pages/ui/messages'
 import Tabs from './pages/ui/tabs'
+import Gallery from './pages/ui/gallery'
+import Carousel from './pages/ui/carousel'
 import NoMatch from './pages/nomatch'
 
 export default class MRouter extends React.Component{
@@ -28,6 +30,8 @@ export default class MRouter extends React.Component{
                 <Route path="/admin/ui/notification" component={Notice} />
                 <Route path="/admin/ui/messages" component={Messages} />
                 <Route path="/admin/ui/tabs" component={Tabs} />
+                <Route path="/admin/ui/gallery" component={Gallery} />
+                <Route path="/admin/ui/carousel" component={Carousel} />
                 <Route component={NoMatch} />
               </Switch>
             </Admin>
